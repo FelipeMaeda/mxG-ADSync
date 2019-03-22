856ec6954776b19a2b7c81592621baa5588d070e
dazoliveira <douglaszanquetta@hotmail.com>
2019-03-22 18:29:41 -0300

    pass exception

--- db_instances/views.py
+++ db_instances/views.py
@@ -10,41 +10,13 @@ from decouple import config
 def index(request):
     try:
         cnx = mysql.connector.connect(user='', password='',
-                                      host='',
-                                      database='')
+                                      host='', database='')
         print('Connect to dabase successful')
 
         cursor = cnx.cursor()
         query = ("SELECT domain, address, user, `password`, base, directory_type "
                  "FROM domain_adldap")
         cursor.execute(query)
-
-        for (domain, address, user, password, base, directory_type) in cursor:
-            if directory_type == 'zimbra':
-                print('DOMAIN: ', domain, ' -> ZIMBRA')
-                # in mxhero DB domain_aldap TABLE address is SEVER
-                SERVER = address
-                USER = user
-                PASSWORD = password
-                BASE = base
-
-                total_entries = 0
-                server = Server(SERVER, get_info=ALL)
-                conn = Connection(server, USER, PASSWORD, auto_bind=True)
-                conn.search(BASE, '(displayName=*)',
-                            attributes=['displayName', 'mail'])
-                total_entries += len(conn.response)
-
-                if total_entries > 0:
-                    for entry in conn.response:
-                        print('ATTRIBUTES: ', entry['attributes'])
-                else:
-                    print('NO RESULTS FOR: ', domain)
-
-                conn.unbind()
-                print('####### NEXT #######')
-            else:
-                print('DOMAIN', domain, ' -> NOT ZIMBRA')
 
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -53,8 +25,35 @@ def index(request):
             print("Database does not exist")
         else:
             print(err)
-    else:
-        cnx.close()
+
+    for (domain, address, user, password, base, directory_type) in cursor:
+        try:
+            if directory_type == 'zimbra':
+                print('DOMAIN: ', domain, ' -> ZIMBRA')
+                # in mxhero DB domain_aldap TABLE address is SEVER
+                SERVER = address
+                USER = user
+                PASSWORD = password
+                BASE = base
+                total_entries = 0
+                server = Server(SERVER, get_info=ALL)
+                conn = Connection(server, USER, PASSWORD, auto_bind=True)
+                conn.search(BASE, '(displayName=*)',
+                            attributes=['displayName', 'mail'])
+                total_entries += len(conn.response)
+                if total_entries > 0:
+                    for entry in conn.response:
+                        print('ATTRIBUTES: ', entry['attributes'])
+                else:
+                    print('NO RESULTS FOR: ', domain)
+                conn.unbind()
+                print('####### NEXT #######')
+            else:
+                print('DOMAIN', domain, ' -> NOT ZIMBRA')
+        except:
+            pass
+    cnx.close()
+
     # result = add.delay()
     # print("CELERY TEST: ",result.get())
 
