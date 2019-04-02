64b3bf024e43b920a7be637970ea453318483c39
dazoliveira <douglaszanquetta@hotmail.com>
2019-04-02 16:21:54 -0300

    implements schedule creation

--- mxG_ADSync/urls.py
+++ mxG_ADSync/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('credenciais/', include('db_instances.urls')),
+    path('instancias/', include('db_instances.urls')),
     path('admin/', admin.site.urls),
 ]
